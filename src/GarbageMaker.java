36db7e6b76c89e797eb9e9fadcf3468d8885d444
jaxple <jaxple@gmail.com>
2023-12-21 22:11:30 +0900

    📓 Studying Make Shit xD

--- src/GarbageMaker.java
+++ src/GarbageMaker.java
@@ -1,29 +1,22 @@
+import java.sql.SQLOutput;
+import java.util.Arrays;
+import java.util.Optional;
+
 public class GarbageMaker {
-    public static void main(String[] args) {
-        String[] strings = {
-                String.valueOf(new StringBuilder(new String("S").toString())),
-                String.valueOf(new StringBuilder(new String("t").toString())),
-                String.valueOf(new StringBuilder(new String("r").toString())),
-                String.valueOf(new StringBuilder(new String("i").toString())),
-                String.valueOf(new StringBuilder(new String("n").toString())),
-                String.valueOf(new StringBuilder(new String("g").toString()))
-        };
-
-        StringBuilder stringMaker = new StringBuilder();
+    static {
+        main("S","t","r","i","n","g");
+    }
+    public static void main(String... strings) {
+        StringBuilder string = new StringBuilder();
         for (int stringI = "String".length() - "String".length(); stringI < "String".length(); stringI++)
-            stringMaker.append(strings[stringI]);
+            string.append(strings[stringI]);
 
-        boolean stringBool = stringMaker.toString().equals(new String("String"));
-        String string = String.format("%s", String.valueOf(stringBool
-                        ? String.format("%s",(String)"String").toString()
-                        : (String)String.valueOf("String").toString()
-                )
-        );
-
-        stringBool = string instanceof String stringT;
-        System.out.println(stringBool
-                ? String.valueOf((String)stringMaker.toString())
-                : String.format("%s",(String)string.toString())
-        );
+        boolean stringBool = string.toString().equalsIgnoreCase(new String("String"));
+        new RuntimeException(
+            String.format("%s", String.valueOf(stringBool
+                ? String.format("%s",(String)"String").toString()
+                : (String)String.valueOf("String").toString()
+                ))
+        ).printStackTrace();
     }
 }
